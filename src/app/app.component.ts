8d8223e8c9f3347c88faa1be4dcdd95b8cb16e1a
Praveen <praveenchary.kokkonda@gmail.com>
2025-05-21 15:52:06 +0530

    template updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,27 +7,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterModule, NgbModule],
-  template: `
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary mb-4">
-      <div class="container">
-        <a class="navbar-brand" routerLink="/">Angular Shop</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link" routerLink="/" routerLinkActive="active">Home</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-
-    <div class="container">
-      <router-outlet></router-outlet>
-    </div>
-  `,
+  templateUrl: './app.component.html',
   styles: []
 })
 export class AppComponent {}
